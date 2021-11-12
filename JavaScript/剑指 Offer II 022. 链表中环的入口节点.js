758b28cca8f6a9f94f6cd2844fa03f8a04880ab7
hzq8147 <hzq8147@163.com>
2021-11-12 18:10:34 +0800

    增加快慢指针解法

--- JavaScript/剑指 Offer II 022. 链表中环的入口节点.js
+++ JavaScript/剑指 Offer II 022. 链表中环的入口节点.js
@@ -5,12 +5,15 @@
  *     this.next = null;
  * }
  */
-
+function ListNode(val) {
+      this.val = val;
+      this.next = null;
+  }
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
-var detectCycle = function(head) {
+var detectCycle1 = function(head) {
     const map = new Set();
     let pos = head;
     while (pos && pos.next != null){
@@ -23,12 +26,33 @@ var detectCycle = function(head) {
     }
     return null;
 };
+
+var detectCycle = function(head) {
+    let fast = head,slow = head;
+    while (slow != null && fast != null && fast.next != null){
+        fast = fast.next.next;
+        slow = slow.next;
+        if (fast == slow){
+            fast = head;
+            while (fast != slow){
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+    }
+    return null;
+};
+
+
 a = new ListNode(1);
 b = new ListNode(2);
 a.next = b;
 b.next = a;
 console.log(detectCycle(a));
 /**
- * 链表-hash表
+ * 1.链表-hash表
  * Set和Map结构
+ * 2.快慢指针 快指针是慢指针路程2倍 计算得到 a = c + (n-1)(b+c) 
+ * 相遇之后，快指针回原点 两指针共同next 再相遇即为环起点
  */
